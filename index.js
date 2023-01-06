9364a34d2da5b5575c6529cf528800c61c43690b
abeizer <abby@regression.gg>
2023-01-06 10:34:11 -0500

    [REG-499] revert add matchInfoEmitter to samples

--- index.js
+++ index.js
@@ -3,9 +3,8 @@
  * The Bot will run around and gather Poppies until it has 100 in its inventory.
  *
  * @param {RGBot} bot
- * @param {EventEmitter} matchInfoEmitter
  */
-function configureBot(bot, matchInfoEmitter) {
+function configureBot(bot) {
 
     bot.setDebug(true);
 
